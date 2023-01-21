637d9da3d01c475a96c41d621e152003cba685eb
Eren <erencan_inanci@hotmail.com>
2023-01-21 15:44:47 +0300

    input handling

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ const initialFacts = [
 function App() {
   const appTitle = "TODAY I LEARNED!";
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
@@ -59,11 +60,13 @@ function App() {
         showFormObj={showForm}
       />
 
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -87,8 +90,79 @@ function Header(props) {
   );
 }
 
-function NewFactForm() {
-  return <form className="fact-form">Fact Form</form>;
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm(props) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("http://example.com");
+  const [category, setCategory] = useState("");
+
+  function handleSubmit(e) {
+    //1=Prevent browser reload
+    e.preventDefault();
+    console.log(text, source, category);
+
+    //2=Check if the data is valid if so create new fact
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+      //3=Create fact object
+      const newFact = {
+        id: Math.trunc(Math.random() * 100000),
+        text: text,
+        source: source,
+        category: category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      //4=Add the new fact to the UI: add the fact to state (new fact + previous facts)
+      props.setFacts((facts) => [newFact, ...facts]);
+
+      //5=Reset input fiels to empty
+      setText("");
+      setSource("");
+      setCategory("");
+
+      //6=Close the form
+      props.setShowForm((show) => !show); //or just false
+    }
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - text.length}</span>
+      <input
+        type="text"
+        placeholder="Thrustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose Category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
 }
 
 function CategoryFilter() {
@@ -113,13 +187,11 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
-  //TEMP
-  const facts = initialFacts;
+function FactList(props) {
   return (
     <section>
       <ul className="fact-list">
-        {facts.map((fact) => (
+        {props.facts.map((fact) => (
           <Fact key={fact.id} fact={fact} />
         ))}
       </ul>
