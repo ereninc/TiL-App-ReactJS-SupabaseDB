6a420e8ec6e6f8a0d88a2bb3dd235851594b050e
Eren <erencan_inanci@hotmail.com>
2023-01-21 03:06:47 +0300

    React update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
 function App() {
+  const appTitle = "TODAY I LEARNED!";
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      {/* HEADER */}
+      <Header
+        titleObj={appTitle}
+        setShowFormObj={setShowForm}
+        showFormObj={showForm}
+      />
+
+      {showForm ? <NewFactForm /> : null}
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function Header(props) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./logo.png" alt="Today I Learned Logo" />
+        <h1>{props.titleObj}</h1>
+      </div>
+
+      <button
+        className="btn btn-large btn-share"
+        onClick={() => props.setShowFormObj((show) => !show)}
+      >
+        {props.showFormObj ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+function FactList() {
+  //TEMP
+  const facts = initialFacts;
+  return (
+    <section>
+      <ul className="fact-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Fact(fact) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.fact.text}
+        <a className="source" href={fact.fact.source}>
+          (Source)
         </a>
-      </header>
-    </div>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find(
+            (cat) => cat.name === fact.fact.category
+          ).color,
+        }}
+      >
+        {fact.fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.fact.votesInteresting}</button>
+        <button>🤯 {fact.fact.votesMindblowing}</button>
+        <button>⛔️ {fact.fact.votesFalse}</button>
+      </div>
+    </li>
   );
 }
 
